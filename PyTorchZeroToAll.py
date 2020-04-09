b69b2d16ecc3b945022b4068c8067917036eb2be
smill <smiller@canioves.com>
2020-04-09 10:27:48 -0400

    Add PyTorchZeroToAll.py (helperCode)

---
+++ PyTorchZeroToAll.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 08:40:06 2020
+
+@author: smill
+
+https://www.youtube.com/watch?v=SKq-pmkekTk&list=PLlMkM4tgfjnJ3I-dbhO9JTw7gNty6o_2m
+"""
+
+import torch
+
+def main():
+    """
+    param xx : xx
+    return : xx : xx
+    -------
+    """
+    
+    Exercise_1_1()    
+
+def Exercise_1_1():
+    """
+    param n/a : n/a
+    return n/a : n/a
+    -------
+    """
+    
+    print(torch.__version__)     
+        
+if __name__ == '__main__':
+    # execute only if run as the entry point into the program
+    main()
